fbc29158112b43e5af8f06517eff8d1862129033
Anton <antontertyshnik@mail.ru>
2023-03-16 16:31:15 +0300

    Add captcha

--- src/main/java/com/example/twitterclone/entity/User.java
+++ src/main/java/com/example/twitterclone/entity/User.java
@@ -19,9 +19,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password can not be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation can not be empty")
-    private String password2;
     private boolean active;
     @Email(message = "Email is not correct")
     @NotBlank(message = "Email can not be empty")
@@ -89,6 +86,4 @@ public class User implements UserDetails {
     public void setEmail(String email) { this.email = email; }
     public String getActivationCode() { return activationCode; }
     public void setActivationCode(String activationCode) { this.activationCode = activationCode; }
-    public String getPassword2() { return password2; }
-    public void setPassword2(String password2) { this.password2 = password2; }
 }
